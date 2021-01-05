3343b84bc50c5f72caeb8290adbdf78e778c89d9
Lucas Lee <lucasclee11@gmail.com>
2021-01-04 22:09:35 -0500

    changes to search tables

--- opportunities.js
+++ opportunities.js
@@ -1,23 +1,31 @@
-document.getElementById("search").addEventListener("keyup", filter);
+window.onload = function(){
+    var elective = document.getElementById("elective_search");
+    var opp = document.getElementById("opp_search");
 
-function filter() {
+    if(elective) elective.addEventListener("keyup", function(){
+        filter("elective_search", "elective_table");
+    });
+    if(opp) opp.addEventListener("keyup", function(){
+        filter("opp_search", "opp_table");
+    });
 
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("search");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("oppTable");
-  tr = table.getElementsByTagName("tr");
+}
 
 
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
+function filter(source, target) {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById(source);
+    filter = input.value.toUpperCase();
+    table = document.getElementById(target);
+    tr = table.getElementsByTagName("tr");
+
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) tr[i].style.display = "";
+            else tr[i].style.display = "none";
+        }
     }
-  }
 }
