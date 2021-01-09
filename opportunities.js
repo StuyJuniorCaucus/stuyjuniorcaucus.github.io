910a25a970cd84d529f9a2ec442eddd0c30e2645
Lucas Lee <lucasclee11@gmail.com>
2021-01-09 14:31:39 -0500

    sheets api connected

--- opportunities.js
+++ opportunities.js
@@ -1,13 +1,15 @@
 window.onload = function(){
     var elective = document.getElementById("elective_search");
     var opp = document.getElementById("opp_search");
+    var opp_table = document.getElementById("opp_table");
 
     if(elective) elective.addEventListener("keyup", function(){
         filter("elective_search", "elective_table");
     });
     if(opp) opp.addEventListener("keyup", function(){
-        filter("opp_search", "opp_table");
+            filter("opp_search", "opp_table");
     });
+    if(opp_table) readData(opp_table);
 
 }
 
@@ -28,4 +30,45 @@ function filter(source, target) {
             else tr[i].style.display = "none";
         }
     }
+}
+
+var spData = null;
+function doData(json) {
+    spData = json.feed.entry;
+}
+
+function drawCell(tr, val) {
+    var td = document.createElement("td")
+    tr.append(td);
+    td.append(val);
+    return td;
+}
+
+function drawRow(table, rowData) {
+    if (rowData == null) return null;
+    if (rowData.length == 0) return null;
+
+    var tr = document.createElement("tr");
+    table.append(tr);
+
+    for(var c = 0; c < rowData.length; c++) {
+        drawCell(tr, rowData[c]);
+    }
+    return tr;
+}
+
+
+function readData(parent) {
+    var data = spData;
+    var rowData = [];
+    for(var r = 1; r < data.length; r++) {
+        var cell = data[r]["gs$cell"];
+        var val = cell["$t"];
+        if (cell.col == 1) {
+            drawRow(parent, rowData);
+            rowData = [];
+        }
+        if(cell.row != 1) rowData.push(val);
+  }
+  drawRow(parent, rowData);
 }
