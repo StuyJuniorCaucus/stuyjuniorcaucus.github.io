05fcfc15d48389e5c403192dbb47e70dac2917ac
Victor Veytsman <vityavv@gmail.com>
2021-01-22 17:57:29 -0500

    Make opportunities.js more ES6-like

--- opportunities.js
+++ opportunities.js
@@ -1,36 +1,30 @@
-window.onload = function(){
-    var elective = document.getElementById("elective_search");
-    var opp = document.getElementById("opp_search");
-
-    if(elective) elective.addEventListener("keyup", function(){
-        filter("elective_search", "elective_table");
-    });
-    if(opp) opp.addEventListener("keyup", function(){
-        filter("opp_search", "opp_table");
-    });
-
+window.onload = () => {
+	Array.from(document.querySelectorAll(".searchable_table")).forEach(table => {
+		const input = document.getElementById(table.dataset.search_id)
+		input.addEventListener("keyup", () => {
+			filter(table, input);
+		});
+	});
 }
 
 
-function filter(source, target) {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById(source);
-    filter = input.value.toUpperCase();
-    table = document.getElementById(target);
-    tr = table.getElementsByTagName("tr");
+function filter(table, input) {
+	const filter = input.value.toUpperCase();
+	const rows = table.getElementsByTagName("tr");
+	const filterBy = JSON.parse(table.dataset.filtered_columns);
 
-
-    for (i = 0; i < tr.length; i++) {
-        td1 = tr[i].getElementsByTagName("td")[1];
-        td2 = tr[i].getElementsByTagName("td")[0];
-        if (td1||td2) {
-            txtValue1 = td1.textContent || td1.innerText;
-            txtValue2 = td2.textContent || td2.innerText;
-            if (txtValue1.toUpperCase().indexOf(filter) > -1||txtValue2.toUpperCase().indexOf(filter) > -1){
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-    }
+	Array.from(rows).forEach(row => {
+		const includesFilter = filterBy.some(column => {
+			const cell = row.getElementsByTagName("td")[column];
+			if (!cell) return true;
+			let text = cell.textContent || cell.innerText;
+			text = text.toUpperCase();
+			return text.includes(filter);
+		})
+		if (includesFilter) {
+			row.style.display = "";
+		} else {
+			row.style.display = "none";
+		}
+	})
 }
