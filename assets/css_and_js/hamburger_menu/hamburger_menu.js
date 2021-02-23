beb0aba590f9431ebce5dc923d8beda000e55b06
rengzhengcodes <rzhengcontacts@gmail.com>
2021-02-22 21:47:59 -0500

    Instead of changing state, it adds the active/inactive class.

--- assets/css_and_js/hamburger_menu/hamburger_menu.js
+++ assets/css_and_js/hamburger_menu/hamburger_menu.js
@@ -1,8 +1,4 @@
 function hamburgerMenu() {
 	var container = document.getElementById("navbarLinks");
-	if (container.style.display === "flex") {
-		container.style.display = "none";
-	} else {
-		container.style.display = "flex";
-	}
+	container.classList.toggle('active');
 }
